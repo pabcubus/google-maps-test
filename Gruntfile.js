99b88c664f61b9bdd9f93b0ef1040b4be91e6539
Pablo Bassil <pabcubus@gmail.com>
2017-06-21 00:30:09 -0500

    no message

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,24 +10,7 @@ module.exports = function(grunt) {
 			},
 			dist: {
 				files: {
-					'app/styles/login.css': 'app/styles/login.scss',
-					'app/styles/alerts.css': 'app/styles/alerts.scss',
-					'app/styles/cognitive_building.css': 'app/styles/cognitive_building.scss',
-					'app/styles/emergency_lighting.css': 'app/styles/emergency_lighting.scss',
-					'app/styles/energy_overview.css': 'app/styles/energy_overview.scss',
-					'app/styles/overview.css': 'app/styles/overview.scss',
-					'app/styles/left_menu.css': 'app/styles/left_menu.scss',
-					'app/styles/shared.css': 'app/styles/shared.scss',
-					'app/styles/site_popup.css': 'app/styles/site_popup.scss',
-					'app/styles/site_tabs.css': 'app/styles/site_tabs.scss',
-					'app/styles/toolbar.css': 'app/styles/toolbar.scss',
-					'app/styles/messages.css': 'app/styles/messages.scss',
-					'app/styles/environmental_overview.css': 'app/styles/environmental_overview.scss',
-					'app/styles/asset_list.css': 'app/styles/asset_list.scss',
-					'app/styles/asset_management.css': 'app/styles/asset_management.scss',
-					'app/styles/device_info_popup.css': 'app/styles/device_info_popup.scss',
-					'app/styles/screen_info.css': 'app/styles/screen_info.scss',
-					'app/styles/settings_popup.css': 'app/styles/settings_popup.scss'
+					'app/styles/shared.css': 'app/styles/shared.scss'
 				}
 			}
 		},
---
+++ app/setup/init.js
@@ -0,0 +1,1 @@
+var app = angular.module('google-maps-test', ['ngLodash', 'ngMaterial']);
