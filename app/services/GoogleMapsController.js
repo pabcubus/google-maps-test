17934d125c9d8be676a51c23d3dbd0cf25849218
Pablo Bassil <pabcubus@gmail.com>
2017-06-22 10:00:45 -0500

    no message

--- app/services/GoogleMapsController.js
+++ app/services/GoogleMapsController.js
@@ -1,4 +1,6 @@
-app.service('GoogleMapsController', function(lodash) {
+'use strict'
+app.service('GoogleMapsController', function($q, lodash, DataController) {
+
 	var vm = this;
 
 	vm.selectedMarkers = [];
@@ -6,6 +8,7 @@ app.service('GoogleMapsController', function(lodash) {
 	vm.selectedMarkersPoints = null;
 	vm.createMap = createMap;
 	vm.setSite = setSite;
+	vm.markers = [];
 	vm.centerMap = centerMap;
 	vm.markerType = {
 		site: {
@@ -64,8 +67,11 @@ app.service('GoogleMapsController', function(lodash) {
 				marker.addListener('click', function() {
 					infowindow.open(map, marker);
 					addSelectedMarker(map, marker);
+					addUnicornPath(map, marker);
 				});
 			}
+
+			vm.markers.push(marker);
 
 			return marker;
 		}
@@ -81,11 +87,58 @@ app.service('GoogleMapsController', function(lodash) {
 		map.fitBounds(bounds);
 	}
 
+	function addUnicornPath(map, marker) {
+		var promises = [];
+
+		if (vm.unicornPath) {
+			vm.unicornPath.setMap(null);
+		}
+
+		vm.markers.forEach(function(mark, index) {
+			var maxDistance = null;
+			var distance = getDistanceFromLatLonInKm(marker.position.lat(), marker.position.lng(), mark.position.lat(), mark.position.lng());
+
+			if ((0 < distance) && ((distance < maxDistance) || (maxDistance == null))){
+				maxDistance = distance;
+				vm.unicornPathPoints = [
+					{
+						lat: marker.position.lat(),
+						lng: marker.position.lng(),
+					},
+					{
+						lat: mark.position.lat(),
+						lng: mark.position.lng(),
+					}
+				];
+			}
+		});
+
+		vm.unicornPath = new google.maps.Polyline({
+			path: vm.unicornPathPoints,
+			geodesic: true,
+			strokeColor: '#FF0000',
+			strokeOpacity: 1.0,
+			strokeWeight: 0,
+			icons: [{
+				icon: {
+					path: google.maps.SymbolPath.CIRCLE,
+					scale: 8,
+					strokeColor: '#393'
+				},
+				offset: '100%'
+			}]
+		});
+
+		vm.unicornPath.setMap(map);
+
+		animateCircle(vm.unicornPath);
+	}
+
 	function addSelectedMarker(map, marker) {
 		if (vm.selectedMarkers.length >= 2) {
 			vm.selectedMarkersPoints = [];
 			vm.selectedMarkersPath.setMap(null);
-			lodash.forEach(vm.selectedMarkers, function(mark){
+			lodash.forEach(vm.selectedMarkers, function(mark) {
 				mark.setIcon(vm.markerType.site.url);
 			});
 			vm.selectedMarkers = [];
@@ -93,17 +146,14 @@ app.service('GoogleMapsController', function(lodash) {
 		marker.setIcon(vm.markerType.site_bad.url);
 		vm.selectedMarkers.push(marker);
 
-
 		if (vm.selectedMarkers.length == 2) {
 			vm.selectedMarkersPoints = [];
 
-			lodash.forEach(vm.selectedMarkers, function(mark){
-				vm.selectedMarkersPoints.push(
-					{
-						lat: mark.position.lat(),
-						lng: mark.position.lng(),
-					}
-				);
+			lodash.forEach(vm.selectedMarkers, function(mark) {
+				vm.selectedMarkersPoints.push({
+					lat: mark.position.lat(),
+					lng: mark.position.lng(),
+				});
 			});
 
 			vm.selectedMarkersPath = new google.maps.Polyline({
@@ -116,5 +166,33 @@ app.service('GoogleMapsController', function(lodash) {
 
 			vm.selectedMarkersPath.setMap(map);
 		}
+	}
+
+	function animateCircle(line) {
+		var count = 0;
+		window.setInterval(function() {
+			count = (count + 1) % 200;
+
+			var icons = line.get('icons');
+			icons[0].offset = (count / 2) + '%';
+			line.set('icons', icons);
+		}, 20);
+	}
+
+	function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+		var R = 6371; // Radius of the earth in km
+		var dLat = deg2rad(lat2 - lat1); // deg2rad below
+		var dLon = deg2rad(lon2 - lon1);
+		var a =
+			Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+			Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+			Math.sin(dLon / 2) * Math.sin(dLon / 2);
+		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		var d = R * c; // Distance in km
+		return d;
+	}
+
+	function deg2rad(deg) {
+		return deg * (Math.PI / 180)
 	}
 });
